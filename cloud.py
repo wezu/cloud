b40adc277f719525b2473097ecaa4911ea8c381e
wezu <grzechotnik1984@gmail.com>
2014-07-14 18:21:14 +0200

    clouds can have color, shader improvment(?)

--- cloud.py
+++ cloud.py
@@ -11,7 +11,7 @@ class World(DirectObject):
         cloud = NodePath(cloud_lod)
         lod0 = loader.loadModel("cloud")
         lod0.reparentTo(cloud)
-        cloud_lod.addSwitch(590, 0)
+        cloud_lod.addSwitch(600, 0)
         shader = loader.loadShader("clouds.cg")
         cloud_lod.setFadeTime(10.0)
         self.sky=loader.loadModel("sky")
@@ -24,10 +24,11 @@ class World(DirectObject):
         #config here!            
         self.cloud_x=1200
         self.cloud_y=1200
-        self.cloud_z=20        
-        self.cloud_speed=0.3
+        self.cloud_z=30        
+        self.cloud_speed=0.5
         cloud_size=5
         cloud_count=50
+        cloud_color=(.9,.9,1, 1.0)
         self.clouds=[]
         
         for i in range(cloud_count):
@@ -37,7 +38,7 @@ class World(DirectObject):
             self.clouds[-1].setShaderInput("offset", Vec4(random.randrange(5)*0.25, random.randrange(9)*0.125, 0, 0))
             self.clouds[-1].setShader(shader)
             self.clouds[-1].setBillboardPointEye()
-        
+            self.clouds[-1].setColor(cloud_color)
         
         self.time=0
         self.uv=Vec4(0, 0, 0.25, 0)        
@@ -59,16 +60,16 @@ class World(DirectObject):
             if self.uv[0]>1.0:
                 self.uv[0]=0
                 self.uv[1]+=0.125
-                if self.uv[1]>1.0:
-                    self.uv=Vec4(0, 0, 0.25, 0)
+                #if self.uv[1]>1.0:
+                #    self.uv=Vec4(0, 0, 0, 0)
         if self.time<0.0:
             self.cloud_speed*=-1.0
             self.uv[2]+=0.5
             if self.uv[2]>1.0:
                 self.uv[2]=0.25
                 self.uv[3]+=0.125
-                if self.uv[3]>1.0:
-                    self.uv=Vec4(0, 0, 0.25, 0)
+                #if self.uv[3]>1.0:
+                #    self.uv=Vec4(0, 0, 0, 0)
         render.setShaderInput("time", self.time)
         render.setShaderInput("uv", self.uv)     
         return task.again
